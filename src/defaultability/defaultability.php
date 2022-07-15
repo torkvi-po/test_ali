5ee04b0b738ab112132eec06e2b4d5ece7cf3e23
Pavel Ivanou <89636018+torkvi-po@users.noreply.github.com>
2022-07-15 14:14:49 +0300

    new

---
+++ src/defaultability/defaultability.php
@@ -0,0 +1,22 @@
+<?php
+namespace Topsdk\Topapi\Defaultability;
+
+use Topsdk\Topapi\TopApiClient;
+use Topsdk\Topapi\Defaultability\Request\TaobaoKfcKeywordSearchRequest;
+
+class Defaultability {
+
+    public $client;
+
+    function __construct(TopApiClient $client) {
+        $this->client = $client;
+    }
+
+
+    /**
+        关键词过滤匹配
+    **/
+    public function taobaoKfcKeywordSearch(TaobaoKfcKeywordSearchRequest $request,string $session) {
+        return $this->client->executeWithSession("taobao.kfc.keyword.search", $request->toMap(), $request->toFileParamMap(), $session);
+    }
+}
